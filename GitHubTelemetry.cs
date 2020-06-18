b9d701b7283424e597b8ec1329fc0ab0551d65cf
Gabriel <mrgabriel.galarza@gmail.com>
2020-06-18 01:13:15 -0400

    Add files via upload

---
+++ GitHubTelemetry.cs
@@ -0,0 +1,35 @@
+﻿using GitHubTelemetry.Services;
+
+namespace GitHubTelemetry
+{
+    /// <summary>
+    /// Startup entry point 
+    /// </summary>
+    public class GitHubTelemetry
+    {
+        private GitHubService gitHubService;
+        private ExcelService excelService;
+        private AzureBlobService blobService;
+
+        /// <summary>
+        /// Dependency injected constructor with necessary services
+        /// </summary>
+        /// <param name="injectedGitHubService"></param>
+        /// <param name="injectedExcelService"></param>
+        public GitHubTelemetry(GitHubService injectedGitHubService, ExcelService injectedExcelService, AzureBlobService injectedBlobService)
+        {
+            gitHubService = injectedGitHubService;
+            excelService = injectedExcelService;
+            blobService = injectedBlobService;
+        }
+
+        public void Run()
+        {
+            // Capture and export github data
+            string fileName = excelService.ExportPullRequestData(gitHubService.GetMergedPullRequests());
+
+            // Upload it to blob storage
+            blobService.UploadBlob(fileName);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using GitHubTelemetry.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace GitHubTelemetry
+{
+    class Program
+    {      
+        static void Main(string[] args)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddJsonFile("settings.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            IServiceCollection services = new ServiceCollection();
+            services.AddSingleton(configuration);
+            services.AddTransient<ExcelService>();
+            services.AddTransient<GitHubService>();
+            services.AddTransient<GitHubTelemetry>();
+            services.AddTransient<AzureBlobService>();
+
+            ServiceProvider serviceProvider = services.BuildServiceProvider();
+
+            serviceProvider.GetService<GitHubTelemetry>().Run();
+
+            Console.ReadLine();
+        }
+    }
+}
